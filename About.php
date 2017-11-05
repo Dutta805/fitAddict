abb8b75976086d91492717a7404fd53ddd3c24fd
Dutta805 <dutta.sid789686@gmail.com>
2017-11-06 00:57:04 +0530

    newly added About page

---
+++ About.php
@@ -0,0 +1,140 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="icon" type="image/png" href="images/fitAddict.png">
+<link rel="stylesheet" type="text/css" href="css/About.css">
+<title>fitAddict</title>
+</head>
+<body>
+
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+<div class="container">
+<button class="navbar-toggler navbar-toggler-right" data-toggle="collapse" data-target="#navbarNav"><span class="navbar-toggler-icon"></span></button>
+
+<div class="collapse navbar-collapse" id="navbarNav">
+<a href="#" class="navbar-brand text-muted"><img height="60px" width="60px" src="images/fitAddict.png" alt=""></a>
+	<ul class="navbar-nav ml-auto">
+	<li class="nav-item p-2">
+	<a class="nav-link" href="index.php">HOME</a>
+	</li>
+	<li class="nav-item p-2">
+	<a class="nav-link" href="consult.php">CONSULT</a>
+	</li>
+	<li class="nav-item p-2">
+	<a class="nav-link" href="#blog-head-section">BLOG</a>
+	</li>
+	<li class="nav-item p-2 active">
+	<a class="nav-link" href="About.php">ABOUT US</a>
+	</li>
+	<li class="nav-item p-2">
+	<a class="nav-link" href="#as-head-section">LOGIN</a>
+	</li>
+	</ul>
+</div>
+</div>
+</nav>
+<br/>
+<br/>
+<br/>
+<!-- header -->
+<header id="page" class="py-4">
+	<div class="container">
+	<div class="row">
+	<div class="col-md-6 offset-md-3 text-center">
+	<br/><br/>
+		<h1 class="display-4">About Us</h1>
+		<p class="lead">we care for your health so that you can take care of your future</p>
+	</div>
+	</div>
+	</div>
+</header>
+
+<section id="about">
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 align-self-center mt-4 text-muted">
+			<h1>What we do</h1>
+			<p>Our company is an initiative to change the lifestyle of our clients. A study published in the noted medical journal Lancet says India is just behind US and China in this global hazard list of top 10 countries with highest number of obese people. In order to change this scenarior, We have developed hundreds of fitness plans and diet plans for our wide range of clients. Here we provide you direct consultation with all the top experts in the field of fitness, nutrition and chiroprators. Moreover we make sure that you achieve your fitness goals not by imposing but by motivation.</p><br/>
+			<p><q>We help you with your fitness goals, so that you can help yourself achieve your goals</q></p>
+		</div>
+		<div class="col-md-6">
+			<img height="400px" width="400px" src="images/dp.jpg" alt="" class="about-img img-fluid rounded-circle hidden-sm-down">
+		</div>
+	</div>
+</div>	
+</section>
+
+<!-- icon boxes -->
+<section id="icon-boxes" class="p-5">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+			<a href="#">
+			<div class="card card-inverse card-danger text-center text-white">
+			<div class="card-block">
+				<i class="fa fa-clock-o"></i>
+				<br/><br/>
+				<h3>24*7 Technical Support</h3>
+				Our staff are online 24*7 to assist to your problems.
+			</div>	
+			</div></a>
+			</div>
+
+			<div class="col-md-4">
+			<a href="#">
+			<div class="card card-inverse card-danger text-center text-white">
+			<div class="card-block">
+				<i class="fa fa-bullhorn"></i>
+				<br/><br/>
+				<h3>FAQs</h3>
+				Find out some frequently asked Questions and Answers.
+			</div>
+				
+			</div></a>
+				
+			</div>
+
+			<div class="col-md-4">
+			<a href="#">
+			<div class="card card-inverse card-danger text-center text-white">
+			<div class="card-block">
+				<i class="fa fa-vcard-o"></i><br/><br/>
+				<h3>Contact Us</h3>
+				Our contact details and address are provided here
+			</div>
+				
+			</div></a>
+				
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- footer -->
+<section id="footer" class="text-inverse py-3">
+	<div id="back" class="container">
+	<div class="d-flex flex-row align-items-center justify-content-between">
+				<h1 class="display-6">Follow Us</h1>
+				<div><a href="#"><i class="fa fa-twitter fa-inverse p-4"></a></i></div>
+				<div><a href="#"><i class="fa fa-facebook fa-inverse p-4"></a></i></div>
+				<div><a href="#"><i class="fa fa-instagram fa-inverse p-4"></a></i></div>
+				<div><a href="#"><i class="fa fa-linkedin fa-inverse p-4"></a></i></div>
+				<small class="small text-right"><div class="fa fa-copyright">fitAddict 2017</div></small>
+            </div>
+		</div>
+</section>
+
+
+<script src="https://code.jquery-3.2.1.min.js" ></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+</body>
+</html>
